adac1dd4738109b6dc31ed72b1d7659fa8e924eb
viprgupta <viprgupta@paypal.com>
2020-02-07 11:30:44 -0800

    fixing a typo

--- utils/file/file.go
+++ utils/file/file.go
@@ -228,7 +228,7 @@ func WriteChangeToFiles(ctx context.Context) error {
 	for file := range filesMap {
 		content, err := yaml.Marshal(filesMap[file])
 		if err != nil {
-			log.Errorf("error occured while unmarshalling file from fileMap: %s", err)
+			log.Errorf("error occured while marshalling file from fileMap: %s", err)
 		}
 		_, err = WriteToFile(file.(string), content)
 		if err != nil {
