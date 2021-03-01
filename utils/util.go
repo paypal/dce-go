4d22ac6cf305771eb3b4ea78541b1c9e886ff494
Harinee Soundarapandian <hsoundarapandian@paypal.com>
2021-03-01 17:21:43 -0600

    Prining logs

--- utils/util.go
+++ utils/util.go
@@ -66,7 +66,9 @@ SetStepData sets the metricsof the DCE step in the map. If the step already exis
 */
 func SetStepData(stepData map[interface{}]interface{}, startTime, endTime int64, stepName, status string) {
 
-	log.Info("STep Name" + stepName)
+	log.Info("Step Name :" + stepName)
+	log.Info("Start Time :" + stepName)
+	log.Info("End Time :" + stepName)
 
 	if len(stepName) == 0 {
 		log.Error("error while updating step data for Granular Metrics: step name can't be empty for stepData")
